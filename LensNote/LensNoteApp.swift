b421020bb996bfe75d0cc016a4decac8038403ce
kito2007 <kito200742@gmail.com>
2025-12-18 14:35:16 +0900

    Initial Commit

---
+++ LensNote/LensNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LensNoteApp.swift
+//  LensNote
+//
+//  Created by 박태영 on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LensNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
