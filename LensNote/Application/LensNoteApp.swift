95f6b393ad289f5fee0ff13f16046a217a3ec81b
kito2007 <kito200742@gmail.com>
2026-03-19 20:44:46 +0900

    앱 엔트리 포인트에서 DIContainer를 이용해 의존성 주입

--- LensNote/Application/LensNoteApp.swift
+++ LensNote/Application/LensNoteApp.swift
@@ -8,21 +8,20 @@
 import SwiftUI
 
 /// 앱 진입점.
-/// 공용 repository 인스턴스를 한 번만 만들고 각 탭 ViewModel에 주입한다.
+/// DIContainer를 이용해 카메라 기능 관련 의존성을 추가한다.
 @main
 struct LensNoteApp: App {
-
-    // ✅ 하나의 Repository 인스턴스를 공유해야
-    // Camera에서 저장한 데이터가 지도 갤러리에도 반영됨
-    private let photoRepository = LocalPhotoRepository()
-
+    
+    private let container: DIContainer
+    private let cameraVM: CameraViewModel
+    
+    init(){
+        self.container = DIContainer()
+        self.cameraVM = container.makeCameraViewModel()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            // 저장 유스케이스를 Camera ViewModel에 주입
-            let saveUseCase = SavePhotoUseCase(repository: photoRepository)
-
-            let cameraVM = CameraViewModel(savePhotoUseCase: saveUseCase)
-
             RootView(cameraVM: cameraVM)
         }
     }
